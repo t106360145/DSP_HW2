43a533e79d1691772a11449b5ea466f947bca24a
t106360145 <55581074+t106360145@users.noreply.github.com>
2019-12-09 17:03:46 +0800

    modify

--- Hugo_filter.m
+++ Hugo_filter.m
@@ -1,8 +1,16 @@
 function [out, t]=Hugo_filter(h,x)
         out=zeros(1,length(x)+length(h)-1); %convolution length
-        for i = 1:length(x)
-            for k= 1: length(h)
-                    out(i+k-1)=out(i+k-1)+x(i)*h(k);
+        for i = 1:length(out)
+            for k= 1:length(h)
+                if(i-k>0)
+                    if(i-k+1)<=length(x)
+                        out(i)=out(i)+h(k)*x(i-k+1);
+                    else
+                        continue;
+                    end
+                else
+                    break;
+                end
             end
         end
     t=1:length(out);
